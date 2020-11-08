8e47b45130e40b226a6de95179e0ca4135fb7297
Eddie Saunders <saunders.eddie@outlook.com>
2020-11-07 21:17:28 -0500

    Added in API calls: getMetroStsatus, getBusStatus, getLocalWeather

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,23 @@
 // EXS 7th November 2020 edwyn.saunders@outlook.com
 
 console.log ("Script Loaded")
+$ (document).ready(function() {
+
+    console.log ("Inside JQuery")
+
+    const getMetroStatus = (METRO-API) => {
+        return ("fnork!");
+        // console.log ("Inside Metro Status");
+    }
+
+    const getBusStatus = (BUS-API) => {
+        console.log ("Inside Bus Status");
+    }
+
+    const getLocalWeather = (WEATHER-API) => {
+
+    }
+    console.log (getMetroStatus());
+    getBusStatus();
+    // End of JQuery 
+})
